c2e227f2b095694ff2be887c81569a80d0232509
Edwin Elia <edwinelia@google.com>
2018-11-30 11:20:35 -0800

    fix build

--- cdap-ui/app/directives/react-components/index.js
+++ cdap-ui/app/directives/react-components/index.js
@@ -65,6 +65,7 @@ angular.module(PKG.name + '.commons')
   })
   .directive('toggleSwitch', function(reactDirective) {
     return reactDirective(window.CaskCommon.ToggleSwitch);
+  })
   .directive('pipelineListView', function(reactDirective) {
     return reactDirective(window.CaskCommon.PipelineList);
   });
